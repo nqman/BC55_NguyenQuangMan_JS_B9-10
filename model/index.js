a5237eaf1fb946f746a4799f5d644de9071182fa
quangman <quangmank16@gmail.com>
2023-09-24 00:50:40 +0700

    update

--- model/index.js
+++ model/index.js
@@ -1,1 +1,66 @@
-alert(123);
+// Tạo đối tượng dsnv từ lớp đối tượng DanhSachNhanVien
+var dsnv = new DanhSachNhanVien();
+
+function domID(id) {
+  return document.getElementById(id);
+}
+
+// Lấy thông tin nhân viên
+function layThongTinNV() {
+  var _taiKhoan = domID("tknv").value;
+  var _hoTen = domID("name").value;
+  var _email = domID("email").value;
+  var _matKhau = domID("password").value;
+  var _ngayLam = domID("datepicker").value;
+  var _luongCoBan = domID("luongCB").value * 1;
+  var _chucVu = domID("chucvu").value;
+  var _gioLam = domID("gioLam").value * 1;
+  // Tạo đối tượng nv từ lớp đối tượng NhanVien
+  var nv = new NhanVien(
+    _taiKhoan,
+    _hoTen,
+    _email,
+    _matKhau,
+    _ngayLam,
+    _luongCoBan,
+    _chucVu,
+    _gioLam
+  );
+  //   Tính lương
+  nv.tinhTongLuong("chucvu");
+  return nv;
+}
+
+// tạo đối tượng nhân viên từ lớp đối tượng NhanVien
+
+// Thêm nhân viên
+function themNguoiDung() {
+  var nv = layThongTinNV();
+  console.log(nv);
+  if (nv) {
+    dsnv._themNV(nv);
+    nv._xepLoai();
+    console.log(nv.gioLam);
+    console.log(dsnv.arr);
+    renderListNV(dsnv.arr);
+  }
+}
+// Render danh sách nhân viên
+function renderListNV(data) {
+  var content = "";
+  for (var i = 0; i < data.length; i++) {
+    nv = data[i];
+    content += `
+      <tr>
+          <td>${nv.taiKhoan}</td>
+          <td>${nv.hoTen}</td>
+          <td>${nv.email}</td>
+          <td>${nv.ngayLam}</td>
+          <td>${nv.chucVu}</td>
+          <td>${nv.tongLuong}</td>
+          <td>${nv.xepLoai}</td>
+      </tr>
+        `;
+  }
+  domID("tableDanhSach").innerHTML = content;
+}
---
+++ model/nhanvien.js
@@ -0,0 +1,53 @@
+function NhanVien(
+  _taiKhoan,
+  _hoTen,
+  _email,
+  _matKhau,
+  _ngayLam,
+  _luongCoBan,
+  _chucVu,
+  _gioLam
+) {
+  // Property
+  this.taiKhoan = _taiKhoan;
+  this.hoTen = _hoTen;
+  this.email = _email;
+  this.matKhau = _matKhau;
+  this.ngayLam = _ngayLam;
+  this.luongCoBan = _luongCoBan;
+  this.chucVu = _chucVu;
+  this.gioLam = _gioLam;
+  this.tongLuong = 0;
+  this.xepLoai = "";
+  //   Method tính tổng lương cho từng đối tượng
+  this.tinhTongLuong = function (idSelect) {
+    var selectIndex = domID(idSelect).selectedIndex;
+    console.log(selectIndex);
+    switch (selectIndex) {
+      case 1:
+        var tongLuong = Number(this.luongCoBan) * 3;
+        break;
+      case 2:
+        var tongLuong = Number(this.luongCoBan) * 2;
+        break;
+      case 3:
+        var tongLuong = Number(this.luongCoBan) * 1;
+        break;
+    }
+    this.tongLuong = tongLuong;
+  };
+  this._xepLoai = function () {
+    var gioLam = this.gioLam;
+    if (gioLam >= 192) {
+      xepLoai = "Nhân viên Xuất Sắc";
+    } else if (192 > gioLam && gioLam >= 176) {
+      xepLoai = "Nhân viên Giỏi";
+    } else if (176 > gioLam && gioLam >= 160) {
+      xepLoai = "Nhân viên Khá";
+    } else if (160 > gioLam) {
+      xepLoai = "Nhân viên Trung Bình";
+    }
+    this.xepLoai = xepLoai;
+    console.log(this.xepLoai);
+  };
+}
