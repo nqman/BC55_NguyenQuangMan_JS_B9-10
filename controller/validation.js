1a45f42c564aae5def11b373c56286ad2df61858
wanman26 <quangmank16@gmail.com>
2023-09-25 00:26:40 +0700

    remaining789

--- controller/validation.js
+++ controller/validation.js
@@ -1,1 +1,86 @@
-
+// Kiểm tra dữ liệu đã nhập hay chưa
+function kiemTraRong(value, idCanhBao, mess) {
+  if (value == "") {
+    domID(idCanhBao).innerHTML = mess;
+    domID(idCanhBao).style.display = "block";
+    return false;
+  } else {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  }
+}
+// Kiểm tra độ dài ký tự
+function doDaiKyTu(value, min, max, idCanhBao, mess) {
+  if (value.length < min || value.length > max) {
+    domID(idCanhBao).innerHTML = mess;
+    domID(idCanhBao).style.display = "block";
+    return false;
+  } else {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  }
+}
+// Kiểm tra định dạng chữ
+function kiemTraChu(value, idCanhBao, mess) {
+  for (var i = 0; i < value.length; i++) {
+    if (!isNaN(value[i])) {
+      domID(idCanhBao).innerHTML = mess;
+      domID(idCanhBao).style.display = "block";
+      return false;
+    }
+  }
+  domID(idCanhBao).innerHTML = "";
+  return true;
+}
+// Kiểm tra định dạng email
+function kiemTraEmail(email, idCanhBao, mess) {
+  var regexEmail =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  var isValid = regexEmail.test(email);
+  if (isValid) {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  } else {
+    domID(idCanhBao).innerHTML = mess;
+    //?
+    domID(idCanhBao).style.display = "block";
+    return false;
+  }
+}
+// Kiểm tra mật khẩu
+function kiemTraMatKhau(value, idCanhBao, mess) {
+  var regexPass = /^(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/;
+  var isValid = regexPass.test(value);
+  console.log(isValid);
+  if (isValid) {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  } else {
+    domID(idCanhBao).innerHTML = mess;
+    domID(idCanhBao).style.display = "block";
+    return false;
+  }
+}
+// Kiểm tra khoảng giá trị (lương cơ bản & số giờ làm)
+function kiemTraKhoangGiaTri(value, min, max, idCanhBao, mess) {
+  if (value < min || value > max) {
+    domID(idCanhBao).innerHTML = mess;
+    domID(idCanhBao).style.display = "block";
+    return false;
+  } else {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  }
+}
+// Kiểm tra chức vụ không để trống
+function kiemTraChucVu(idSelect, idCanhBao, mess) {
+  var selectIndex = domID(idSelect).selectedIndex;
+  if (selectIndex === 0) {
+    domID(idCanhBao).innerHTML = mess;
+    domID(idCanhBao).style.display = "block";
+    return false;
+  } else {
+    domID(idCanhBao).innerHTML = "";
+    return true;
+  }
+}
--- model/index.js
+++ model/index.js
@@ -15,6 +15,58 @@ function layThongTinNV() {
   var _luongCoBan = domID("luongCB").value * 1;
   var _chucVu = domID("chucvu").value;
   var _gioLam = domID("gioLam").value * 1;
+
+  //! Validation
+
+  var isValid = true;
+  // Kiểm tra Tài khoản
+  isValid &=
+    kiemTraRong(_taiKhoan, "tbTKNV", "Vui lòng không bỏ trống") &&
+    doDaiKyTu(_taiKhoan, 4, 6, "tbTKNV", "Vui lòng nhập từ 4-6 ký số");
+  //Kiểm tra tên nhân viên
+  isValid &=
+    kiemTraRong(_hoTen, "tbTen", "Vui lòng không bỏ trống") &&
+    kiemTraChu(_hoTen, "tbTen", "Vui lòng nhập chữ");
+  // Kiểm tra email
+  isValid &=
+    kiemTraRong(_email, "tbEmail", "Vui lòng không bỏ trống") &&
+    kiemTraEmail(_email, "tbEmail", "Vui lòng đúng định dạng email");
+  // Kiểm tra Mật khẩu
+  isValid &=
+    kiemTraRong(_matKhau, "tbMatKhau", "Vui lòng không bỏ trống") &&
+    doDaiKyTu(_matKhau, 6, 10, "tbMatKhau", "Vui lòng nhập từ 6-10 ký tự") &&
+    kiemTraMatKhau(
+      _matKhau,
+      "tbMatKhau",
+      "Mật khẩu phải chứa 1 chữ hoa, 1 chữ số, 1 ký tự đặc biệt"
+    );
+
+  // Kiểm tra Lương cơ bản
+  isValid &=
+    kiemTraRong(_luongCoBan, "tbLuongCB", "Vui lòng không bỏ trống") &&
+    kiemTraKhoangGiaTri(
+      _luongCoBan,
+      1000000,
+      20000000,
+      "tbLuongCB",
+      "Vui lòng nhập mức lương từ 1 triệu -> 20 triệu"
+    );
+  // Kiểm tra chức vụ
+  isValid &= kiemTraChucVu("chucvu", "tbChucVu", "Vui lòng chọn chức vụ");
+  // Kiểm tra số giờ làm
+  isValid &=
+    kiemTraRong(_gioLam, "tbGiolam", "Vui lòng không bỏ trống") &&
+    kiemTraKhoangGiaTri(
+      _gioLam,
+      80,
+      200,
+      "tbGiolam",
+      "Vui lòng nhập số giờ làm từ 80 -> 200 giờ"
+    );
+  if (!isValid) {
+    return null;
+  }
+  //! Validation
   // Tạo đối tượng nv từ lớp đối tượng NhanVien
   var nv = new NhanVien(
     _taiKhoan,
@@ -59,6 +111,10 @@ function renderListNV(data) {
           <td>${nv.chucVu}</td>
           <td>${nv.tongLuong}</td>
           <td>${nv.xepLoai}</td>
+          <td>
+          <button class="btn btn-success" onclick="suaNV('${nv.taiKhoan}')">Sửa</button>
+          <button class="btn btn-danger" onclick="xoaNV('${nv.taiKhoan}')">Xoá</button>
+          </td>
       </tr>
         `;
   }
